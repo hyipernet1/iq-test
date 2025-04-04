a2964d3079225f3bc0832466ed4fb018de02736e
hyipernet1 <hyipernet1@gmail.com>
2025-04-04 14:15:56 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Faq from "@/components/home/faq";
 import Hero from "@/components/home/hero";
 import Plans from "@/components/home/plans";
+import iPhonePromo from "@/components/home/iPhonePromo";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -11,8 +12,10 @@ export default function Home() {
   return (
     <section>
       <Hero />
+      <iPhonePromo />  {/* 👈 оце вставляємо */}
       <Faq />
       <Plans />
     </section>
   );
 }
+
