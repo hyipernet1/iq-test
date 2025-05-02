b2584ef367a38df91ca5c14d2c04391053c86989
ostapenko-kostia <ostapenkokpersonal@gmail.com>
2025-05-02 19:51:46 +0300

    fix

--- src/app/api/(api-routes)/auth/generate-user/route.ts
+++ src/app/api/(api-routes)/auth/generate-user/route.ts
@@ -60,10 +60,10 @@ const generateUser = async ({ email }: { email: string }) => {
 
   const resend = new Resend(process.env.RESEND_API_KEY);
 
-  const emailRes = await resend.emails.send({
-    from: "IQ Logic <onboarding@resend.dev>",
+  await resend.emails.send({
+    from: "My IQ Rank <onboarding@myiqrank.com>",
     to: [email],
-    subject: "Creating Account in IQ Logic",
+    subject: "Creating Account in My IQ Rank",
     html: `<p>You are creating account. <br /><br /> Your Email: ${email} <br/> Your Password: ${password} <br /><br /> Please, save it. We can not recover it!</p>`,
   });
 
